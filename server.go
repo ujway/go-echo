8813d33ea2a9ff0851a6833b2ee459df996cc4fe
ujway <satoyu313@gmail.com>
2018-01-06 18:44:31 +0900

    add api route

--- server.go
+++ server.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"net/http"
-	
+	"strconv"
+
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "query, 200 OK")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/users/:id", func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		return c.JSON(http.StatusOK, getUser(id))
+	})
+	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func getUser(id int) *User {
+	name := "myname"
+	u := &User{
+		Id:   id,
+		Name: name,
+	}
+	return u
+}
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
